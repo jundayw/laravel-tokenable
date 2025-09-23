2e1f53b72acf5d133b49882726e68f4d6546890a
jundayw <jundayw@126.com>
2025-09-23 21:41:07 +0800

    migrations

---
+++ database/migrations/2025_06_01_000000_create_auth_token_table.php
@@ -0,0 +1,53 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration {
+
+    /**
+     * Get the migration connection name.
+     *
+     * @return string|null
+     */
+    public function getConnection(): ?string
+    {
+        return config('tokenable.database.connection');
+    }
+
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up(): void
+    {
+        Schema::create(config('tokenable.database.table', 'auth_tokens'), function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->string('name')->index('idx_name')->comment('Token Name');
+            $table->string('platform')->default('default')->index('idx_platform')->comment('Platform Type');
+            $table->morphs('tokenable', 'idx_tokenable');
+            $table->string('token_driver', 128)->index('idx_token_driver')->nullable()->comment('Token Driver');
+            $table->string('access_token', 128)->index('idx_access_token')->nullable()->comment('Access Token');
+            $table->string('refresh_token', 128)->index('idx_refresh_token')->nullable()->comment('Refresh Token');
+            $table->longText('scopes')->comment('Token Scopes');
+            $table->timestamp('access_token_expire_at')->index('idx_access_token_expire_at')->comment('Token Expiration Time');
+            $table->timestamp('refresh_token_available_at')->index('idx_refresh_token_available_at')->comment('Refresh Token Available Time');
+            $table->timestamp('refresh_token_expire_at')->index('idx_refresh_token_expire_at')->comment('Refresh Token Expiration Time');
+            $table->timestamp('created_at')->nullable()->comment('Created Time');
+            $table->timestamp('updated_at')->nullable()->comment('Updated Time');
+            $table->timestamp('deleted_at')->index('idx_deleted_at')->nullable()->comment('Deleted Time');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists(config('tokenable.database.table', 'auth_tokens'));
+    }
+};
