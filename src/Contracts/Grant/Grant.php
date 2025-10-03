8e1738da4b8bf6b0624387a933abe84b93520fcd
jundayw <jundayw@126.com>
2025-10-03 23:47:46 +0800

    Grant

--- src/Contracts/Grant/Grant.php
+++ src/Contracts/Grant/Grant.php
@@ -2,23 +2,48 @@
 
 namespace Jundayw\Tokenable\Contracts\Grant;
 
+use Illuminate\Contracts\Cache\Repository;
+use Jundayw\Tokenable\Contracts\Auth\Authenticable;
 use Jundayw\Tokenable\Contracts\Auth\TokenableAuthGuard;
+use Jundayw\Tokenable\Contracts\Blacklist;
+use Jundayw\Tokenable\Contracts\Token\Factory as TokenFactoryContract;
+use Jundayw\Tokenable\Contracts\Token\Token;
+use Jundayw\Tokenable\Contracts\Tokenable;
+use Jundayw\Tokenable\Contracts\Whitelist;
 
 interface Grant
 {
-    /**
-     * Get the current grant instance.
-     *
-     * @return TokenableAuthGuard|null
-     */
-    public function getGuard(): ?TokenableAuthGuard;
+    public function getAuthentication(): Authenticable;
 
-    /**
-     * Set the grant instance to be used.
-     *
-     * @param TokenableAuthGuard $guard
-     *
-     * @return static
-     */
-    public function usingGuard(TokenableAuthGuard $guard): static;
+    public function setAuthentication(Authenticable $authentication): static;
+
+    public function getTokenManager(): TokenFactoryContract;
+
+    public function setTokenManager(TokenFactoryContract $tokenManager): static;
+
+    public function getBlacklist(): Blacklist;
+
+    public function setBlacklist(Blacklist $blacklist): static;
+
+    public function getWhitelist(): Whitelist;
+
+    public function setWhitelist(Whitelist $whitelist): static;
+
+    public function getRepository(): Repository;
+
+    public function setRepository(Repository $repository): static;
+
+    public function getTokenable(): Tokenable;
+
+    public function setTokenable(Tokenable $tokenable): static;
+
+    public function getToken(): Token;
+
+    public function setToken(Token $token): static;
+
+    public function withToken(string $token = null): static;
+
+    public function getGuard(): TokenableAuthGuard;
+
+    public function withGuard(TokenableAuthGuard $guard): static;
 }
--- src/Grants/Grant.php
+++ src/Grants/Grant.php
@@ -2,33 +2,119 @@
 
 namespace Jundayw\Tokenable\Grants;
 
+use Illuminate\Contracts\Cache\Repository;
+use Jundayw\Tokenable\Contracts\Auth\Authenticable;
 use Jundayw\Tokenable\Contracts\Auth\TokenableAuthGuard;
+use Jundayw\Tokenable\Contracts\Blacklist;
+use Jundayw\Tokenable\Contracts\Token\Factory as TokenFactoryContract;
+use Jundayw\Tokenable\Contracts\Token\Token;
+use Jundayw\Tokenable\Contracts\Tokenable;
+use Jundayw\Tokenable\Contracts\Whitelist;
 
 abstract class Grant
 {
+    protected Authenticable        $authentication;
+    protected TokenFactoryContract $tokenManager;
+    protected Blacklist            $blacklist;
+    protected Whitelist            $whitelist;
+    protected Repository           $repository;
+
+    public function getAuthentication(): Authenticable
+    {
+        return $this->authentication;
+    }
+
+    public function setAuthentication(Authenticable $authentication): static
+    {
+        $this->authentication = $authentication;
+        return $this;
+    }
+
+    public function getTokenManager(): TokenFactoryContract
+    {
+        return $this->tokenManager;
+    }
+
+    public function setTokenManager(TokenFactoryContract $tokenManager): static
+    {
+        $this->tokenManager = $tokenManager;
+        return $this;
+    }
+
+    public function getBlacklist(): Blacklist
+    {
+        return $this->blacklist;
+    }
+
+    public function setBlacklist(Blacklist $blacklist): static
+    {
+        $this->blacklist = $blacklist;
+        return $this;
+    }
+
+    public function getWhitelist(): Whitelist
+    {
+        return $this->whitelist;
+    }
+
+    public function setWhitelist(Whitelist $whitelist): static
+    {
+        $this->whitelist = $whitelist;
+        return $this;
+    }
+
+    public function getRepository(): Repository
+    {
+        return $this->repository;
+    }
+
+    public function setRepository(Repository $repository): static
+    {
+        $this->repository = $repository;
+        return $this;
+    }
+
+    protected Tokenable $tokenable;
+    protected Token     $token;
+
+    public function getTokenable(): Tokenable
+    {
+        return $this->tokenable;
+    }
+
+    public function setTokenable(Tokenable $tokenable): static
+    {
+        $this->tokenable = $tokenable;
+        return $this;
+    }
+
+    public function getToken(): Token
+    {
+        return $this->token ?? $this->getTokenManager()->driver();
+    }
+
+    public function setToken(Token $token): static
+    {
+        $this->token = $token;
+        return $this;
+    }
+
+    public function withToken(string $token = null): static
+    {
+        $this->token = $this->getTokenManager()->driver($token);
+        return $this;
+    }
+
     protected TokenableAuthGuard $guard;
 
-    /**
-     * Get the current grant instance.
-     *
-     * @return TokenableAuthGuard|null
-     */
-    public function getGuard(): ?TokenableAuthGuard
+    public function getGuard(): TokenableAuthGuard
     {
         return $this->guard;
     }
 
-    /**
-     * Set the grant instance to be used.
-     *
-     * @param TokenableAuthGuard $guard
-     *
-     * @return static
-     */
-    public function usingGuard(TokenableAuthGuard $guard): static
+    public function withGuard(TokenableAuthGuard $guard): static
     {
         $this->guard = $guard;
-
         return $this;
     }
 }
