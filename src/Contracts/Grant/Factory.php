a651b0f4750d7457a025696fedce025064266e12
jundayw <jundayw@126.com>
2025-10-03 15:42:58 +0800

    GrantManager

--- src/Contracts/Grant/Factory.php
+++ src/Contracts/Grant/Factory.php
@@ -12,59 +12,9 @@ interface Factory
     public function getAccessTokenGrant(): AccessTokenGrant;
 
     /**
-     * Set the AccessTokenGrant instance.
-     *
-     * @param AccessTokenGrant $accessTokenGrant
-     *
-     * @return static
-     */
-    public function setAccessTokenGrant(AccessTokenGrant $accessTokenGrant): static;
-
-    /**
      * Get the AuthorizationCodeGrant instance.
      *
      * @return AuthorizationCodeGrant
      */
     public function getAuthorizationCodeGrant(): AuthorizationCodeGrant;
-
-    /**
-     * Set the AuthorizationCodeGrant instance.
-     *
-     * @param AuthorizationCodeGrant $authorizationCodeGrant
-     *
-     * @return $this
-     */
-    public function setAuthorizationCodeGrant(AuthorizationCodeGrant $authorizationCodeGrant): static;
-
-    /**
-     * Get the RefreshTokenGrant instance.
-     *
-     * @return RefreshTokenGrant
-     */
-    public function getRefreshTokenGrant(): RefreshTokenGrant;
-
-    /**
-     * Set the RefreshTokenGrant instance.
-     *
-     * @param RefreshTokenGrant $refreshTokenGrant
-     *
-     * @return static
-     */
-    public function setRefreshTokenGrant(RefreshTokenGrant $refreshTokenGrant): static;
-
-    /**
-     * Get the RevokeTokenGrant instance.
-     *
-     * @return RevokeTokenGrant
-     */
-    public function getRevokeTokenGrant(): RevokeTokenGrant;
-
-    /**
-     * Set the RevokeTokenGrant instance.
-     *
-     * @param RevokeTokenGrant $revokeTokenGrant
-     *
-     * @return static
-     */
-    public function setRevokeTokenGrant(RevokeTokenGrant $revokeTokenGrant): static;
 }
--- src/GrantManager.php
+++ src/GrantManager.php
@@ -4,16 +4,12 @@ namespace Jundayw\Tokenable;
 
 use Jundayw\Tokenable\Contracts\Grant\AccessTokenGrant;
 use Jundayw\Tokenable\Contracts\Grant\AuthorizationCodeGrant;
-use Jundayw\Tokenable\Contracts\Grant\RefreshTokenGrant;
-use Jundayw\Tokenable\Contracts\Grant\RevokeTokenGrant;
 
 class GrantManager implements Contracts\Grant\Factory
 {
     public function __construct(
         protected AccessTokenGrant $accessTokenGrant,
         protected AuthorizationCodeGrant $authorizationCodeGrant,
-        protected RefreshTokenGrant $refreshTokenGrant,
-        protected RevokeTokenGrant $revokeTokenGrant,
     ) {
         //
     }
@@ -29,19 +25,6 @@ class GrantManager implements Contracts\Grant\Factory
     }
 
     /**
-     * Set the AccessTokenGrant instance.
-     *
-     * @param AccessTokenGrant $accessTokenGrant
-     *
-     * @return static
-     */
-    public function setAccessTokenGrant(AccessTokenGrant $accessTokenGrant): static
-    {
-        $this->accessTokenGrant = $accessTokenGrant;
-        return $this;
-    }
-
-    /**
      * Get the AuthorizationCodeGrant instance.
      *
      * @return AuthorizationCodeGrant
@@ -49,64 +32,5 @@ class GrantManager implements Contracts\Grant\Factory
     public function getAuthorizationCodeGrant(): AuthorizationCodeGrant
     {
         return $this->authorizationCodeGrant;
-    }
-
-    /**
-     * Set the AuthorizationCodeGrant instance.
-     *
-     * @param AuthorizationCodeGrant $authorizationCodeGrant
-     *
-     * @return $this
-     */
-    public function setAuthorizationCodeGrant(AuthorizationCodeGrant $authorizationCodeGrant): static
-    {
-        $this->authorizationCodeGrant = $authorizationCodeGrant;
-        return $this;
-    }
-
-    /**
-     * Get the RefreshTokenGrant instance.
-     *
-     * @return RefreshTokenGrant
-     */
-    public function getRefreshTokenGrant(): RefreshTokenGrant
-    {
-        return $this->refreshTokenGrant;
-    }
-
-    /**
-     * Set the RefreshTokenGrant instance.
-     *
-     * @param RefreshTokenGrant $refreshTokenGrant
-     *
-     * @return static
-     */
-    public function setRefreshTokenGrant(RefreshTokenGrant $refreshTokenGrant): static
-    {
-        $this->refreshTokenGrant = $refreshTokenGrant;
-        return $this;
-    }
-
-    /**
-     * Get the RevokeTokenGrant instance.
-     *
-     * @return RevokeTokenGrant
-     */
-    public function getRevokeTokenGrant(): RevokeTokenGrant
-    {
-        return $this->revokeTokenGrant;
-    }
-
-    /**
-     * Set the RevokeTokenGrant instance.
-     *
-     * @param RevokeTokenGrant $revokeTokenGrant
-     *
-     * @return static
-     */
-    public function setRevokeTokenGrant(RevokeTokenGrant $revokeTokenGrant): static
-    {
-        $this->revokeTokenGrant = $revokeTokenGrant;
-        return $this;
     }
 }
